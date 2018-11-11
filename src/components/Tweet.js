90c660ac71d98deec18ef214280e96825d74b2c7
Rodrigo Santos <ro.augusto@gmail.com>
2018-11-11 13:36:07 -0200

    update timeline

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
+import like from '../like.svg'
 import './Tweet.css'
+import api from '../services/api';
 
 export default class Tweet extends Component {
+
+  handleLike = async () =>{
+    const { _id } = this.props.tweet;
+
+    await api.post(`likes/${_id}`)
+  }
+
   render() {
-    return <h1>{this.props.tweet.content}</h1>
+    const { tweet } = this.props;
+    return (
+      <li className="tweet">
+        <strong>{tweet.author}</strong>
+        <p>{tweet.content}</p>
+        <button type="button" onClick={this.handleLike}>
+          <img src={like} alt="Like" />
+          {tweet.likes}
+        </button>
+      </li>
+    )
   }
 }
